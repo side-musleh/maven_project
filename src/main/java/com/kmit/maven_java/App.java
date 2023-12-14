97f368c24d80b6c01908de99227326ca12c42df1
side-musleh <147480169+side-musleh@users.noreply.github.com>
2023-12-14 15:38:44 +0530

    Add files via upload

---
+++ src/main/java/com/kmit/maven_java/App.java
@@ -0,0 +1,15 @@
+package com.kmit.maven_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World !" );
+        System.out.println("hello it-b");
+        System.out.println("jenkins");
+    }
+}
